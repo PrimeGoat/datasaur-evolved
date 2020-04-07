3192293a3924545b722bf2f31686a42b0e913c1f
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-04-07 12:47:06 -0400

    Done

--- main.js
+++ main.js
@@ -1,8 +1,140 @@
+const newDinosaur = function(dinosaur = {species: '', period: '', carnivore: '', extinct: false}) {
+  return {
+    species: dinosaur.species,
+    period: dinosaur.period,
+    carnivore: dinosaur.carnivore,
+    extinct: dinosaur.extinct
+  };
+}
+
+const makeDino = function(species, period, carnivore, extinct = false) {
+  return {
+    species: species,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct
+  };
+}
+
+
 /***********************
  * ITERATION FUNCTIONS *
  **********************/
 
+const singularizeDinos = function(dinos) {
+  return dinos.map(function(dinosaur) {
+    const dino = newDinosaur(dinosaur);
+  
+    if(dino.species.match(/us$/)) {
+      dino.species = dino.species.slice(0, -2);
+    }
+  
+    return dino;
+  });
+}
 
+const truncateDinos = function(dinos) {
+  return dinos.map(function(dinosaur) {
+    const dino = newDinosaur(dinosaur);
+  
+    if(dino.species.length > 10) {
+      dino.species = dino.species.slice(0,7) + "...";
+    }
+  
+    return dino;
+  });
+}
+
+const makeAllExtinct = function(dinos) {
+  return dinos.map(function(dinosaur) {
+    const dino = newDinosaur(dinosaur);
+  
+    dino.extinct = true;
+  
+    return dino;
+  });
+}
+
+const carnivoresOnly = function(dinos) {
+  return dinos.filter(function(dinosaur) {
+    return dinosaur.carnivore;
+  });
+}
+
+const herbivoresOnly = function(dinos) {
+  return dinos.filter(function(dinosaur) {
+    return !dinosaur.carnivore;
+  });
+}
+
+const extinctOnly = function(dinos) {
+  return dinos.filter(function(dinosaur) {
+    return dinosaur.extinct;
+  });
+}
+
+const notExtinct = function(dinos) {
+  return dinos.filter(function(dinosaur) {
+    return !dinosaur.extinct;
+  });
+}
+
+const triassicOnly = function(dinos) {
+  return dinos.filter(isTriassic = function(dinosaur) {
+    return dinosaur.period == "Triassic";
+  });
+}
+
+const notTriassic = function(dinos) {
+  return dinos.filter(isTriassic = function(dinosaur) {
+    return dinosaur.period != "Triassic";
+  });
+}
+
+const bySpecies = function(dinos) {
+  newDinos = [...dinos];
+  return newDinos.sort(function(dino1, dino2) {
+    if(dino1.species < dino2.species) {
+      return -1
+    } else if(dino1.species == dino2.species) {
+      return 0;
+    } else if(dino2.species < dino1.species) {
+      return 1;
+    }
+  });
+}
+
+const byExtinctLast = function(dinos) {
+  newDinos = [...dinos];
+  return newDinos.sort(function(dino1, dino2) {
+    return dino1.extinct - dino2.extinct;
+  });
+}
+
+const byCarnivoresFirst = function(dinos) {
+  newDinos = [...dinos];
+  return newDinos.sort(function(dino1, dino2) {
+    return dino2.carnivore - dino1.carnivore;
+  });
+}
+
+const byPeriod = function(dinos) {
+  newDinos = [...dinos];
+  return newDinos.sort(function(dino1, dino2) {
+    return periodToNumber(dino1.period) - periodToNumber(dino2.period);
+  });
+}
+
+const periodToNumber = function(period) {
+  switch(period) {
+    case "Triassic":
+      return 1;
+    case "Jurassic":
+      return 2;
+    case "Cretaceous":
+      return 3;
+  }
+}
 
 /*********************************
  * TEST SETUP CODE - DON'T TOUCH!*
